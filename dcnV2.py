cb20e77eb19e6fc522b237fca680e36c15a719a5
DESKTOP-9EPMHDO\RayKwak <kdgil3415@naver.com>
2020-06-02 10:35:54 +0900

    make grid

--- dcnV2.py
+++ dcnV2.py
@@ -59,3 +59,56 @@ def DeformableConv2D(input,
                                        normalizer_params=None,
                                        scope='weights_conv')
             modulation = tf.sigmoid(modulation)
+
+        input_shape = tf.shape(input)
+
+        # [b, h, w, 2c] -> [b*c, h, w, 2]
+        offsets = tf.transpose(offsets, [0,3,1,2])
+        offsets = tf.reshape(offsets, [offsets.shape[0], -1, 2, offsets.shape[2],  offsets.shape[3]])
+        offsets = tf.transpose(offsets, [0, 1, 3, 4, 2])
+        offsets = tf.reshape(offsets, [-1, offsets.shape[2], offsets.shape[3], 2])
+
+        # [b, h, w, c] -> [b*c, h, w]
+        modulation = tf.transpose(modulation, [0,3,1,2])
+        modulation = tf.reshape(modulation,[-1, modulation.shape[2],  modulation.shape[3]])
+
+        grid_t = tf.reshape(tf.range(input_shape[1]),[-1,1])
+        grid_l = tf.reshape(tf.range(input_shape[2]),[1,-1])
+        grid_b = tf.reshape(tf.range(input_shape[0]),[-1,1,1,1])
+        grid_bk = tf.tile(grid_b,[kernel_size*kernel_size,1,1,1])
+
+        grid_t = tf.tile(grid_t, [1, input_shape[2]])
+        grid_l = tf.tile(grid_l, [input_shape[1], 1])
+        grid_tl = tf.stack([grid_t,grid_l],axis=-1)
+        grid_tl = tf.tile(tf.expand_dims(grid_tl, axis=0), [offsets.shape[0], 1, 1, 1])
+
+        coordi_yx = tf.cast(grid_tl, dtype=tf.float32) + offsets
+        coordi_y_clip = tf.clip_by_value(coordi_yx[...,0],
+                                         clip_value_min=0.,
+                                         clip_value_max=tf.cast(input_shape[1], dtype=tf.float32) - 1.)
+        coordi_x_clip = tf.clip_by_value(coordi_yx[..., 1],
+                                         clip_value_min=0.,
+                                         clip_value_max=tf.cast(input_shape[2], dtype=tf.float32) - 1.)
+        coordi_yx = tf.stack([coordi_y_clip,coordi_x_clip], axis=-1)
+        grid_bk = tf.cast(tf.tile(grid_bk, [1, input_shape[1], input_shape[2],1]),dtype=tf.float32)
+        coordi_bkyx = tf.concat([grid_bk, coordi_yx],axis=-1)
+
+        coordi_bktl = tf.cast(tf.floor(coordi_bkyx), dtype=tf.int32)
+        coordi_bkbr = coordi_bktl + tf.constant(1)
+        coordi_bktr = tf.stack([coordi_bktl[...,0], coordi_bktl[...,1], coordi_bkbr[...,2]],axis=-1)
+        coordi_bkbl = tf.stack([coordi_bktl[...,0], coordi_bktl[...,2], coordi_bkbr[...,1]],axis=-1)
+
+        #[batch, kernel*kernel, top, left, channel]
+        var_bktlc = tf.gather_nd(input,tf.reshape(coordi_bktl,[-1,3]))
+        var_tl = tf.reshape(var_bktlc,[input_shape[0]*kernel_size*kernel_size,
+                                          input_shape[1],
+                                          input_shape[2],
+                                          input_shape[3]])
+
+        # [batch, kernel*kernel, top, right, channel]
+        var_bktrc = tf.gather_nd(input, tf.reshape(coordi_bktr, [-1, 3]))
+        var_tr = tf.reshape(var_bktrc, [input_shape[0] * kernel_size * kernel_size,
+                                           input_shape[1],
+                                           input_shape[2],
+                                           input_shape[3]])
+
